9599e352e501e42aa2a1f8f2e995ea9b8871331c
Code-Apprentice <codeguru42@gmail.com>
2025-03-09 17:33:52 -0600

    Try to get URL

--- usps.py
+++ usps.py
@@ -1,7 +1,10 @@
+import httpx
 import typer
 
-def main():
-    ...
+def main(url: str):
+    response = httpx.get(url)
+    typer.echo(f'Status code: {response.status_code}')
+    typer.echo(f'Content: {response.text[:100]}...')
 
 if __name__ == '__main__':
     typer.run(main)
