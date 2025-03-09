d3dc8fef37df5130b6fa0b874bf8112cd9074ba3
Code-Apprentice <codeguru42@gmail.com>
2025-03-09 17:39:17 -0600

    Set User-Agent

--- usps.py
+++ usps.py
@@ -1,10 +1,15 @@
 import httpx
 import typer
 
-def main(url: str):
-    response = httpx.get(url)
-    typer.echo(f'Status code: {response.status_code}')
-    typer.echo(f'Content: {response.text[:100]}...')
 
-if __name__ == '__main__':
+def main(url: str):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36"
+    }
+    response = httpx.get(url, headers=headers)
+    typer.echo(f"Status code: {response.status_code}")
+    typer.echo(f"Content: {response.text[:100]}...")
+
+
+if __name__ == "__main__":
     typer.run(main)
